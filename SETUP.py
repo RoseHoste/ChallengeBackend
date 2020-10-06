aca4a0986f215cde70441ccb520d00bbbee4973c
Hoste Rose <hoste.rose@gmail.com>
2020-10-06 17:35:02 +0200

    Update SETUP.py

--- SETUP.py
+++ SETUP.py
@@ -1,7 +1,7 @@
 POSTGRES ={
-    'user':'groover',
-    'pw':'groover',
-    'db': 'challenge',
+    'user':<username>,
+    'pw':<passwords>,
+    'db': <name of database>,
     'host': 'localhost',
     'port': '5432'
 
@@ -9,5 +9,5 @@ POSTGRES ={
 
 CLIENT_CREDENTIALS = {
     'client_id':'e18d6952d6854b6c9ab1a161a013e6e3',
-    'client_secret':'7555b89676e34ac69a1c32c49b3dfef6'
+    'client_secret': <client secret>
 }
