6800f296a70f0531e26a8a108baadc76d1ddf878
Hoste Rose <hoste.rose@gmail.com>
2020-10-07 15:53:13 +0200

    Update setup.py

--- SETUP.py
+++ SETUP.py
@@ -1,8 +1,8 @@
 #Replace username, password and database name with the appropriate strings
 POSTGRES ={
-    'user': 'groover',
-    'pw': 'groover',
-    'db': 'challenge',
+    'user': <user>,
+    'pw': <password>,
+    'db': <name of the database>,
     'host': 'localhost',
     'port': '5432'
 
@@ -10,7 +10,7 @@ POSTGRES ={
 #Replace <client_secret> with your client secret provided by spotify, as a string
 CLIENT_CREDENTIALS = {
     'client_id':'e18d6952d6854b6c9ab1a161a013e6e3',
-    'client_secret': '7555b89676e34ac69a1c32c49b3dfef6'
+    'client_secret': <client_id>
 }
 
 #Change the limit of new releases fetched, min is 1, max is 50, default is 20
